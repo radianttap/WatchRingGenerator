29c96e7f6610fe57d923fcc56f1a726e80468952
Aleksandar Vacic <av@codeaplus.com>
2015-02-23 15:07:20 +0100

    First commit

--- Watch Ring Generator/ViewController.m
+++ Watch Ring Generator/ViewController.m
@@ -7,8 +7,37 @@
 //
 
 #import "ViewController.h"
+#import "M13ProgressViewSegmentedRing.h"
+@import QuartzCore;
+@import CoreGraphics;
 
-@interface ViewController ()
+@interface ViewController () < UITextFieldDelegate >
+
+@property (weak, nonatomic) IBOutlet UIButton *mm38button;
+@property (weak, nonatomic) IBOutlet UIButton *mm42button;
+@property (weak, nonatomic) IBOutlet UIImageView *watchFace;
+@property (weak, nonatomic) IBOutlet UIView *watchScreen;
+
+@property (weak, nonatomic) IBOutlet M13ProgressViewSegmentedRing *innerRing;
+@property (weak, nonatomic) IBOutlet M13ProgressViewSegmentedRing *outerRing;
+
+@property (weak, nonatomic) IBOutlet UIButton *outerBackgroundButton;
+@property (weak, nonatomic) IBOutlet UIButton *outerForegroundButton;
+@property (weak, nonatomic) IBOutlet UITextField *outerRingSize;
+@property (weak, nonatomic) IBOutlet UITextField *outerRingWidth;
+@property (weak, nonatomic) IBOutlet UITextField *outerRingSegments;
+
+@property (weak, nonatomic) IBOutlet UIButton *innerBackgroundButton;
+@property (weak, nonatomic) IBOutlet UIButton *innerForegroundButton;
+@property (weak, nonatomic) IBOutlet UITextField *innerRingSize;
+@property (weak, nonatomic) IBOutlet UITextField *innerRingWidth;
+@property (weak, nonatomic) IBOutlet UITextField *innerRingSegments;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *watchScreenWidthConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *watchScreenHeightConstraint;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *outerRingWidthConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *innerRingWidthConstraint;
 
 @end
 
@@ -16,12 +45,71 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+	self.outerRing.numberOfSegments = 9;
+	self.outerRing.progressRingWidth = 3.0;
+	self.outerRing.showPercentage = NO;
+
+	self.innerRing.numberOfSegments = 27;
+	self.innerRing.progressRingWidth = 3.0;
+	self.innerRing.showPercentage = NO;
+	
+	self.outerRing.segmentSeparationAngle = self.innerRing.segmentSeparationAngle;
+	[self setupColors];
 }
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+	[super viewDidAppear:animated];
+	
+	[self setGroupProgress:3.0/9.0 sessionProgress:8.0/27.0];
+}
+
+- (void)setGroupProgress:(CGFloat)groupProgress sessionProgress:(CGFloat)sessionProgress {
+	
+	[self.outerRing setProgress:groupProgress animated:YES];
+	[self.innerRing setProgress:sessionProgress animated:YES];
+	
+//	[self.view layoutIfNeeded];
+}
+
+- (void)setupColors {
+	
+	self.outerRing.primaryColor = self.outerForegroundButton.backgroundColor;
+	self.outerRing.secondaryColor = self.outerBackgroundButton.backgroundColor;
+	self.innerRing.primaryColor = self.innerForegroundButton.backgroundColor;
+	self.innerRing.secondaryColor = self.innerBackgroundButton.backgroundColor;
+	
+	[self.watchScreen setNeedsDisplay];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+	
+	if ([textField isEqual:self.outerRingSize]) {
+		self.outerRingWidthConstraint.constant = [textField.text doubleValue];
+		
+	} else if ([textField isEqual:self.outerRingWidth]) {
+		self.outerRing.progressRingWidth = [textField.text doubleValue];
+
+	} else if ([textField isEqual:self.outerRingSegments]) {
+		self.outerRing.numberOfSegments = [textField.text integerValue];
+		
+	} else if ([textField isEqual:self.innerRingSize]) {
+		self.innerRingWidthConstraint.constant = [textField.text doubleValue];
+
+	} else if ([textField isEqual:self.innerRingWidth]) {
+		self.innerRing.progressRingWidth = [textField.text doubleValue];
+		
+	} else if ([textField isEqual:self.innerRingSegments]) {
+		self.innerRing.numberOfSegments = [textField.text integerValue];
+		
+	}
+	
+	self.outerRing.segmentSeparationAngle = self.innerRing.segmentSeparationAngle;
+	[self.outerRing setNeedsDisplay];
+	[self.innerRing setNeedsDisplay];
+	[self.watchScreen layoutIfNeeded];
+	
+	return YES;
 }
 
 @end
